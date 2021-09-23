91b2f658bc119ab49c810e2c8832caa64bfbcd4d
Bponthemove <85110622+Bponthemove@users.noreply.github.com>
2021-09-23 14:28:44 +0100

    again

--- routes/routes.js
+++ routes/routes.js
@@ -1,56 +1,30 @@
 'use strict'
 const path = require('path')
-const chrono = require('chrono-node')
 
 module.exports = app => {
 
     //rendering main
-    app.get('/', (req, res) => {
-        res.sendFile('index.html', {root: path.join(process.cwd() + '/views/')})
-    })
-
-    //user input route
-    app.get("/api/*", function(request, response) {
-        //response.writeHead(200, { "Content-Type": "application/json" });
-        const input = request.url.slice(1)
+    app.get("/api/:date?", (req, res) => {
+        const givenDate = req.params.date
         let date
-        const obj = {
-            natural: null,
-            unix: null
+      
+        // check if no date provided
+        if (!givenDate) {
+          date = new Date()
+        } else {
+          // check if unix time:
+          //    number string multiplied by 1 gives this number, data string gives NaN
+          const checkUnix = givenDate * 1
+          date = isNaN(checkUnix) ? new Date(givenDate) : new Date(checkUnix)
         }
-
-        if  (!isNaN(input)) {
-            date = new Date(parseInt(input,10)*1000)
+      
+        //check if valid format
+        if (date == "Invalid Date") {
+          res.json({ error: "Invalid Date" })
+        } else {
+          const unix = date.getTime()
+          const utc = date.toUTCString()
+          res.json({ unix, utc })
         }
-        else{
-            date = chrono.parseDate(input.replace(new RegExp("%20",'g'),' '));
-        }
-        
-        if (date!= null) {
-            obj.unix = date.getTime()/1000
-            obj.natural = date.toDateString()
-        }
-        
-        response.end(JSON.stringify(obj))
-    }) 
-    // app.get('/api/:dateString?' || '/:datestring?', (req, res) => {
-    //     const { dateString } = req.params
-    //     const timestamp = parseInt(dateString * 1, 10)
-    //     const date = new Date(timestamp || dateString || Date.now())
-    
-    //     let result
-    //     if (isNaN(+date)) {
-    //         result = { error: 'Invalid Date' }
-    //     } else {
-    //         result = {
-    //             unix: date.getTime(),
-    //             utc: date.toUTCString(),
-    //         }
-    //     }
-    //     res.json(result)
-    // })
-
-    // app.get('/api/:day/:month/:year?', (res, req) => {
-        
-    // })
+    })
 }
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 const port = process.env.PORT || 3000
 const routes = require(path.join(process.cwd() + '/routes/routes.js'))
 
-app.use(express.static(path.join(process.cwd() + '/public')))
+app.use(express.static(path.join(process.cwd() + '/views')))
 
 routes(app)
 
