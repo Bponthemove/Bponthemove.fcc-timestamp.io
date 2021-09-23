a6c930c2898969b9c98deaff15eefda9ae3561a5
Bponthemove <85110622+Bponthemove@users.noreply.github.com>
2021-09-23 13:03:50 +0100

    5th attempt

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 'use strict'
 const path = require('path')
+const chrono = require('chrono-node')
 
 module.exports = app => {
 
@@ -8,21 +9,48 @@ module.exports = app => {
         res.sendFile('index.html', {root: path.join(process.cwd() + '/views/')})
     })
 
-    //user input route 
-    app.get('/api/:dateString?', (req, res) => {
-        const { dateString } = req.params
-        const timestamp = parseInt(dateString * 1, 10)
-        const date = new Date(timestamp || dateString || Date.now())
-    
-        let result
-        if (isNaN(+date)) {
-            result = { error: 'Invalid Date' }
-        } else {
-            result = {
-                unix: date.getTime(),
-                utc: date.toUTCString(),
-            }
+    //user input route
+    app.get("*", function(request, response) {
+        //response.writeHead(200, { "Content-Type": "application/json" });
+        const input = request.url.slice(1)
+        let date
+        const obj = {
+            natural: null,
+            unix: null
         }
-        res.json(result)
-    })
+
+        if  (!isNaN(input)) {
+            date = new Date(parseInt(input,10)*1000)
+        }
+        else{
+            date = chrono.parseDate(input.replace(new RegExp("%20",'g'),' '));
+        }
+        
+        if (date!= null) {
+            obj.unix = date.getTime()/1000
+            obj.natural = date.toDateString()
+        }
+        
+        response.end(JSON.stringify(obj))
+    }) 
+    // app.get('/api/:dateString?' || '/:datestring?', (req, res) => {
+    //     const { dateString } = req.params
+    //     const timestamp = parseInt(dateString * 1, 10)
+    //     const date = new Date(timestamp || dateString || Date.now())
+    
+    //     let result
+    //     if (isNaN(+date)) {
+    //         result = { error: 'Invalid Date' }
+    //     } else {
+    //         result = {
+    //             unix: date.getTime(),
+    //             utc: date.toUTCString(),
+    //         }
+    //     }
+    //     res.json(result)
+    // })
+
+    // app.get('/api/:day/:month/:year?', (res, req) => {
+        
+    // })
 }
