a5842e41450aadba60f0105bf1deaa4b7ac7d1e3
Bponthemove <85110622+Bponthemove@users.noreply.github.com>
2021-09-27 11:17:15 +0100

    more cors tries

--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 
 const express = require('express')
 const path = require('path')
-const cors = require('cors')
 
 const app = express()
+
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200})); 
+
 const port = process.env.PORT || 3000
 const routes = require(path.join(process.cwd() + '/routes/routes.js'))
-
-app.use(cors())
 
 app.use(express.static(path.join(process.cwd() + '/views')))
 
